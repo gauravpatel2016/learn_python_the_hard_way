39a848c7b83b8ebba011b4f34114abae87a239f5
Gaurav Patel <gpatel@adobe.com>
2022-01-12 09:11:53 -0700

    Some Exercise 1-15

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb.") 
+print("Its fleece was white as {}.".format('snow')) 
+print("And everywhere that Mary went.") 
+print("." * 10) 
+# what'd that do? 
+end1 = "C" 
+end2 = "h" 
+end3 = "e" 
+end4 = "e" 
+end5 = "s" 
+end6 = "e" 
+end7 = "B" 
+end8 = "u" 
+end9 = "r" 
+end10 = "g" 
+end11 = "e" 
+end12 = "r" # watch end = ' ' at the end. try removing it to see what happens 
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+print('''
+There's something going on here. 
+With the three double-quotes. 
+We'll be able to type as much as we like. 
+Even 4 lines if we want, or 5, or 6. ''')
